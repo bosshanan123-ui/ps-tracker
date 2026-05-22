c2ecb90e140f025d4764ec279381538537287544
Abdul Hanan <bosshanan123@gmail.com>
2026-05-22 20:59:37 +0500

    Update main.cpp
    covert the struct in to the class and add member functions of the class and update the menu that are shown you

--- main.cpp
+++ main.cpp
@@ -1,47 +1,50 @@
 #include<iostream>
 #include<string>
 using namespace std;
+class Transaction {
+private:
+ 
+    int id;
+    string category;
+    float amount;
+    string description;
+    string date;
 
-struct Transaction {
-	int id;
-	string category;
-	float amount;
-	string discription;
-	string date;
-
-};
-Transaction t[50];
-int totalTransactions = 0; 
-
-void addTransaction() {
-    if (totalTransactions >= 50) {
-        cout << "no space in file "<< endl;
-        return;
+public:
+    Transaction() {
+        id = 0;
+        category = "";
+        amount = 0.0;
+        description = "";
+        date = "";
     }
 
-    cout << "Add New Transaction " << endl;
+    Transaction(int i, string cat, float amt, string desc, string d) {
+        id = i;
+        category = cat;
+        amount = amt;
+        description = desc;
+        date = d;
+    }
+    void display() const {
+        cout << "ID: " << id
+            << " | Category: " << category
+            << " | Amount: " << amount
+            << " | Desc: " << description
+            << " | Date: " << date << endl;
+    }
+};
 
-    t[totalTransactions].id = totalTransactions + 1; 
-    
-    cout << "Enter Category ";
-    cin >> t[totalTransactions].category;
-    
-    cout << "Enter Amount: ";
-    cin >> t[totalTransactions].amount;
-    
-    cout << "Enter Description (add anything you ewant to memorize ): ";
-    cin >> t[totalTransactions].description;
-    
-    cout << "Enter Date (DD-MM-YYYY): ";
-    cin >> t[totalTransactions].date;
-
-    totalTransactions++; 
-    cout << "Transaction added successfully" << endl;
-}
+Transaction t[50];
+int transactionCount = 0;
 void menu() {
-	cout << "-------------------------------------------------------" << endl;
-	cout << "             Welcome to personal expense tracker       " << endl;
-	cout << "-------------------------------------------------------" << endl;
+    cout << "\n-------------------------------------------------------" << endl;
+    cout << "             Personal Expense Tracker                    " << endl;
+    cout << "-------------------------------------------------------" << endl;
+    cout << "1. Add a New Expense" << endl;
+    cout << "2. View All Expenses" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter your choice: ";
 }
 int main() {
 	menu();
